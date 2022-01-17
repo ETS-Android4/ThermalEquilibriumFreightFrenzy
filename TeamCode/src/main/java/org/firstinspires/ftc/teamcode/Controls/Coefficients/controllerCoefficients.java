175df663ebde8695841bc431e80d1238b05100da
Ben Caunt <bdcaunt@gmail.com>
2022-01-16 20:29:00 -0500

    Drive to position test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controls/Coefficients/controllerCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controls/Coefficients/controllerCoefficients.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Controls.Coefficients;
 
 public class controllerCoefficients {
-
+	public static final double compBotVelocity = 70;
+	public static final double compBotAcceleration = 90;
+	public static final double compBotJerk = 130;
 	// controller coefficients for competition 10wd
 	public static final PIDFCoefficients compBotTurn = new PIDFCoefficients(1, 0.8, 0.09,0,0,0.1); // 0.2785000000000002
-	public static final PVParams translationCoefficients = new PVParams(1,1,1,1,1,1,1);
+	public static final PVParams translationCoefficients = new PVParams(0.3,0,1/compBotVelocity,0,0,0.04,1);
 	// controller coefficients for off season 6wd
 	public static final PIDFCoefficients protoBotTurn = new PIDFCoefficients(0.59, 0.16, 0.09, 0, 0, 0.0580);
 
@@ -12,9 +14,7 @@ public class controllerCoefficients {
 	public static final PIDFCoefficients slideCoefficients = new PIDFCoefficients(0.007, 0.08, 0);
 
 
-	public static final double compBotVelocity = 70;
-	public static final double compBotAcceleration = 90;
-	public static final double compBotJerk = 130;
+
 
 	public static final double protoBotVelocity = 100;
 	public static final double protoBotAcceleration = 100;
