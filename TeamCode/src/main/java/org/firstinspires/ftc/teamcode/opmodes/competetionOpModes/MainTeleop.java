a3186bcc5f995c48f0e94e3c2c63e461ee1a164e
Ben Caunt <bdcaunt@gmail.com>
2022-01-22 15:37:35 -0500

    Remove the togglebox action.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competetionOpModes/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competetionOpModes/MainTeleop.java
@@ -24,7 +24,7 @@ public class MainTeleop extends BaseTeleop {
 		actions.add(new ToggleIntake(robot, gamepad1, gamepad2));
 		actions.add(new CommandDeposit(robot, gamepad1, gamepad2));
 		actions.add(new GoToCollectionState(robot, gamepad1, gamepad2));
-		actions.add(new ToggleBox(robot, gamepad1, gamepad2));
+		// actions.add(new ToggleBox(robot, gamepad1, gamepad2));
 		actions.add(new ToggleDuckWheel(robot,gamepad1,gamepad2));
 	}
 }
