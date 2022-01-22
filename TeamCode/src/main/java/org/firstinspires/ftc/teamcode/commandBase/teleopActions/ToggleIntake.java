c044ef1da296915f687dc6ac296c776eb91c4135
Ben Caunt <bdcaunt@gmail.com>
2022-01-22 15:31:19 -0500

    Force bucket to go to collection? ???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandBase/teleopActions/ToggleIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandBase/teleopActions/ToggleIntake.java
@@ -34,6 +34,7 @@ public class ToggleIntake implements teleopAction {
 
 		if (robot.Deposit.getState().equals(COLLECTION) || gamepad1.right_trigger > 0.5) {
 			robot.Intake.setState(ON);
+			robot.bucketSys.setState(COLLECTION);
 		} else robot.Intake.setState(OFF);
 
 		if (gamepad1.left_trigger > 0.5) {
