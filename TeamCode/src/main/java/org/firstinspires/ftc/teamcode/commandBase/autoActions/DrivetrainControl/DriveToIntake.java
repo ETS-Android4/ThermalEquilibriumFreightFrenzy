ff5e76f6dcf45fbaf8f844b2c91ce0bca287bea0
Ben Caunt <bdcaunt@gmail.com>
2022-01-23 17:28:53 -0500

    Add DriveToIntake action

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandBase/autoActions/DrivetrainControl/DriveToIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandBase/autoActions/DrivetrainControl/DriveToIntake.java
@@ -2,9 +2,39 @@ package org.firstinspires.ftc.teamcode.commandBase.autoActions.DrivetrainControl
 
 import org.firstinspires.ftc.teamcode.Geometry.Vector3D;
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
+import org.firstinspires.ftc.teamcode.subsystems.scoringMechanisms.Deposit;
+import org.firstinspires.ftc.teamcode.subsystems.scoringMechanisms.Intake;
 
 public class DriveToIntake extends DriveToPosition {
 	public DriveToIntake(Robot robot, Vector3D referencePose, double cutOffTime, boolean useMaxAccel) {
 		super(robot, referencePose, cutOffTime, useMaxAccel);
 	}
+
+	@Override
+	public void startAction() {
+		controller.resetTimer();
+		robot.bucketSys.setCheckSensor(true);
+		robot.bucketSys.setState(Deposit.depositStates.COLLECTION);
+		robot.Deposit.setState(Deposit.depositStates.COLLECTION);
+		robot.Intake.setState(Intake.intakeStates.ON);
+	}
+
+	@Override
+	public boolean isActionPersistent() {
+		return false;
+	}
+
+	@Override
+	public boolean isActionComplete() {
+		return controller.getTime() > cutOffTime || robot.bucketSys.isFreightInBox();
+	}
+	@Override
+	public void stopAction() {
+		robot.driveTrain.STOP();
+		robot.Intake.setState(Intake.intakeStates.OFF);
+		robot.Deposit.setState(Deposit.depositStates.IN);
+		robot.bucketSys.setState(Deposit.depositStates.IN);
+		robot.bucketSys.setCheckSensor(false);
+	}
+
 }
