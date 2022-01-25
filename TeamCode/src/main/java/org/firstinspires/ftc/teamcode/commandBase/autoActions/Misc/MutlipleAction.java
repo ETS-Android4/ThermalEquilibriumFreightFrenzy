fc8f64749c67d45cd52d272a820d74e031be961c
Ben Caunt <bdcaunt@gmail.com>
2022-01-25 01:47:18 -0500

    Ability to run multiple actions at once!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandBase/autoActions/Misc/MutlipleAction.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.commandBase.autoActions.Misc;
+
+import org.firstinspires.ftc.teamcode.commandBase.action;
+
+import java.lang.reflect.Array;
+
+public class MutlipleAction implements action {
+
+    action[] actions;
+
+    public MutlipleAction(action[] actions) {
+        this.actions = actions;
+    }
+
+    @Override
+    public void startAction() {
+
+        for (action a : actions) {
+            a.startAction();
+        }
+
+    }
+
+    @Override
+    public void runAction() {
+        for (action a : actions) {
+            a.runAction();
+        }
+    }
+
+    @Override
+    public void stopAction() {
+
+        for (action a : actions) {
+            a.stopAction();
+        }
+
+    }
+
+    @Override
+    public boolean isActionComplete() {
+
+        for (action a : actions) {
+            if (!a.isActionComplete()) return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isActionPersistent() {
+        return false;
+    }
+}
