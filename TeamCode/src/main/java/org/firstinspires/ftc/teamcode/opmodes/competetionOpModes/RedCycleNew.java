57c6b3813c3bf9a41ca35e8acc5ba16297e929a9
Ben7105 <55768253+Ben7105@users.noreply.github.com>
2022-01-30 22:20:59 -0500

    AUTO WORK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competetionOpModes/RedCycleNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competetionOpModes/RedCycleNew.java
@@ -21,6 +21,7 @@ import org.firstinspires.ftc.teamcode.commandBase.autoActions.Misc.OffsetOdom;
 import org.firstinspires.ftc.teamcode.commandBase.autoActions.SlideControl.DepositFreight;
 import org.firstinspires.ftc.teamcode.commandBase.autoActions.SlideControl.GoToHighDeposit;
 import org.firstinspires.ftc.teamcode.commandBase.autoActions.SlideControl.GoToInState;
+import org.firstinspires.ftc.teamcode.commandBase.teleopActions.SetYPosition;
 import org.firstinspires.ftc.teamcode.opmodes.testOpModes.MultipleActionExample;
 import org.firstinspires.ftc.teamcode.templateOpModes.BaseAuto;
 import org.opencv.core.Mat;
@@ -32,9 +33,9 @@ public class RedCycleNew extends BaseAuto {
 
 	Vector3D start = new Vector3D(TILE / 2.0, -TILE * 3 + 8.375, Math.toRadians(-90));
 
-	Vector3D depositPosition1 = new Vector3D(+ 2,-TILE * 2 + 4 ,Math.toRadians(-65));
+	Vector3D depositPosition1 = new Vector3D(+ 2,-TILE * 2 + 4 ,Math.toRadians(-60));
 
-	Vector3D readyForCollection1 = new Vector3D(TILE - 15, -TILE * 3 + 6,  Math.toRadians(0));
+	Vector3D readyForCollection1 = new Vector3D(TILE - 15, -TILE * 3 + 5.75,  Math.toRadians(0));
 	Vector3D readyForCollection2 = new Vector3D(TILE - 15, -TILE * 3 + 6.5,  Math.toRadians(0));
 	Vector3D readyForCollection3 = new Vector3D(TILE - 15, -TILE * 3 + 6.5,  Math.toRadians(0));
 
@@ -47,17 +48,15 @@ public class RedCycleNew extends BaseAuto {
 	Vector3D gapPose = new Vector3D(TILE, readyForPark.getY(), Math.toRadians(0));
 	Vector3D Test = new Vector3D(TILE / 2.0,-24, Math.toRadians(-90));
 
-	Vector3D LineUp = new Vector3D(TILE - 12, -TILE * 3 + 10,  Math.toRadians(0));
+	Vector3D LineUp = new Vector3D(TILE - 12, -TILE * 3 + 7,  Math.toRadians(0));
 	Vector3D StrafeIntoWall = new Vector3D(0,.5,0);
-	Vector3D newcollect1 = new Vector3D(TILE * 2 - 2,-TILE * 3 + 5.5, Math.toRadians(0));
-	Vector3D newcollect2 = new Vector3D(TILE * 2 ,-TILE * 3 + 5.5, Math.toRadians(0));
-	Vector3D newcollect3 = new Vector3D(TILE * 2 ,-TILE * 3 + 5.5, Math.toRadians(0));
+	Vector3D newcollect1 = new Vector3D(TILE * 2 - 6.5,-TILE * 3 + 5.5, Math.toRadians(0));
+	Vector3D newcollect2 = new Vector3D(TILE * 2 - 4,-TILE * 3 + 5.5, Math.toRadians(0));
+	Vector3D newcollect3 = new Vector3D(TILE * 2 - 1,-TILE * 3 + 5.5, Math.toRadians(0));
 
 	Vector3D NewReadyForDepo1 = new Vector3D(TILE - 15, -TILE * 3 + 5.,  Math.toRadians(0));
 	Vector3D NewReadyForDepo2 = new Vector3D(TILE - 15, -TILE * 3 + 5.25,  Math.toRadians(0));
 	Vector3D NewReadyForDepo3 = new Vector3D(TILE - 15, -TILE * 3 + 5.5,  Math.toRadians(0));
-
-
 
 	protected double turn_cutoff_time_seconds = 0.7;
 
@@ -74,30 +73,39 @@ public class RedCycleNew extends BaseAuto {
 	@Override
 	public void addActions() {
 
-		//Deposit pre-load
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,depositPosition1), new GoToHighDeposit(robot), new DeployIntake(robot)}));
+		//Deposit pre-load Works
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,depositPosition1), new GoToHighDeposit(robot), new DeployIntake(robot),}));
 		actions.add(new DepositFreight(robot));
 		actions.add(new Delay(300));
-		//agaisnt wall
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, LineUp), new GoToInState(robot)}));
-		actions.add(new TimeBasedMove(robot,StrafeIntoWall,.75));
-		//Intake
-		actions.add(new DriveToIntake(robot,newcollect1,3,false));
-		//out of depo
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, NewReadyForDepo1), new TurnOnIntake(robot,false), new Delay(250)}));
+
+		//agaisnt wall Meh
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, LineUp,2,false), new GoToInState(robot)}));
+		actions.add(new TimeBasedMove(robot,StrafeIntoWall,1));
+		actions.add(new SetYPosition(robot,-TILE * 3 + 6 ));
+
+		//Intake Meh goes to far
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,newcollect1), new TurnOnIntake(robot,true),new Delay(500)}));
 		actions.add(new TurnOffIntake(robot));
+		//actions.add(new DriveToIntake(robot,newcollect1,3,false))
+
+		//out of depo
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, NewReadyForDepo1,2,false), new TurnOnIntake(robot,false)}));
+		actions.add(new TurnOffIntake(robot));
+
 //------------------------------------------------------------------------------------------------\\
 		//Deposit pre-load
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,depositPosition1), new GoToHighDeposit(robot), new DeployIntake(robot), new Delay(250)}));
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,depositPosition1), new GoToHighDeposit(robot)}));
 		actions.add(new DepositFreight(robot));
 		actions.add(new Delay(300));
 		//agaisnt wall WITH TIME BASED CODE
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, LineUp), new GoToInState(robot)}));
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, LineUp,2,false), new GoToInState(robot)}));
 		actions.add(new TimeBasedMove(robot,StrafeIntoWall,.75));
+		actions.add(new SetYPosition(robot,-TILE * 3 + 6 ));
 		//Intake
-		actions.add(new DriveToIntake(robot,newcollect2,3,false));
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,newcollect2), new TurnOnIntake(robot,true),new Delay(500)}));
+		actions.add(new TurnOffIntake(robot));
 		//out of depo
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, NewReadyForDepo2), new TurnOnIntake(robot,false)}));
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, NewReadyForDepo2,2,false), new TurnOnIntake(robot,false)}));
 		actions.add(new TurnOffIntake(robot));
 //------------------------------------------------------------------------------------------------\\
 		//Deposit pre-load
@@ -107,13 +115,16 @@ public class RedCycleNew extends BaseAuto {
 
 		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, LineUp), new GoToInState(robot)}));
 		actions.add(new TimeBasedMove(robot,StrafeIntoWall,.75));
+		actions.add(new SetYPosition(robot,-TILE * 3 + 6 ));
 
 		actions.add(new DriveToIntake(robot,newcollect3,3,false));
 
-		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot, NewReadyForDepo3), new TurnOnIntake(robot,false)}));
-		actions.add(new TurnOffIntake(robot));
-
-		/*
+		
+/*
+		//deposit first preload
+		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,depositPosition1), new GoToHighDeposit(robot), new DeployIntake(robot)}));
+		actions.add(new DepositFreight(robot));
+		actions.add(new Delay(300));
 		//Against wall lineup for first warehouse cycle + deploy intake
 		actions.add(new MutlipleAction(new action[]{new DriveToPosition(robot,readyForCollection1) , new GoToInState(robot)}));
 
